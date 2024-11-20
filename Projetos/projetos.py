41bf9ce0573e0be0949ca2feb06d261cac649851
Caio Vinícius <caioviniciuscordeirodoamaral@gmail.com>
2024-11-20 18:17:42 -0300

    Update projetos.py
    Created a new list sorting script.

--- Projetos/projetos.py
+++ Projetos/projetos.py
@@ -350,3 +350,44 @@ for i, (chave, valor) in enumerate(perguntas[n_pergunta].items()):
 
 print(f"Você acertou {pontos}\nde {qttd_perguntas} perguntas\n")
 """
+
+# Ordenador de listas
+"""
+import copy
+
+produtos = [
+    {'nome': 'Produto 5', 'preco': 10.00},
+    {'nome': 'Produto 1', 'preco': 22.32},
+    {'nome': 'Produto 3', 'preco': 10.11},
+    {'nome': 'Produto 2', 'preco': 105.87},
+    {'nome': 'Produto 4', 'preco': 69.90},
+]
+
+# Lista com preço alterado para +10%
+novos_produtos = copy.deepcopy(produtos)
+
+print("Alteração dos valores")
+print("Produto".ljust(15), "Preço".rjust(10))
+print("-"*31)
+
+for n in novos_produtos:
+    print(f"{n["nome"].ljust(20)} R$ {n["preco"]}")
+    n['preco'] = round(n['preco'] * 1.1, 2)
+    print(f"{n["nome"].ljust(20)} R$ {n["preco"]:.2f}\n")
+
+print(f"\nNova lista com valores editados")
+for n in novos_produtos:
+    print(n)
+
+# Lista ordenada por nome decrescente
+produtos_ordenados_por_nome = sorted(novos_produtos, key=lambda x: x["nome"], reverse=True)
+print(f"\nNova lista ordenada por nome")
+for produto in produtos_ordenados_por_nome:
+    print(produto)
+
+# Lista ordenada por preço crescente
+produtos_ordenados_por_preco = sorted(novos_produtos, key=lambda x: x["preco"])
+print(f"\nNova lista ordenada por preço")
+for produto in produtos_ordenados_por_preco:
+    print(produto)
+"""
